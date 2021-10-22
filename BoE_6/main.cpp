db7a186f74b661d726bd6b52901e63f020966617
Anastasiia Zolotova <anastasya.zolotova11@gmail.com>
2021-10-22 21:34:50 +0300

    Initial Commit

---
+++ BoE_6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BoE_6
+//
+//  Created by Anastasya Zolotova on 22.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
