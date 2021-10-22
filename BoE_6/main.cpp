20c7423221965dcaa4736c1064a58cf6092a10ee
Anastasiia Zolotova <anastasya.zolotova11@gmail.com>
2021-10-22 21:39:43 +0300

    main.cpp
    Adding file to the repository.

--- BoE_6/main.cpp
+++ BoE_6/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  BoE_6
-//
-//  Created by Anastasya Zolotova on 22.10.2021.
-//
-
 #include <iostream>
+#include <iomanip>
+#define USE_MATH_DEFINES
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+
+{
+
+    double y=0, x;
+    cout << "Enter x = ";
+    cin >> x;
+    float rez;
+    for (int i = 1; i < 6; i++)
+    {
+        y += i;
+    }
+    y += 2*x;
+    rez = tan(y);
+    cout << "Y=" << rez << endl;
+
     return 0;
 }
